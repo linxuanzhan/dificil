1c8d8f72f5c72655b665086b6f5bfd6b673d4eb0
linxuanzhan <133099133+linxuanzhan@users.noreply.github.com>
2024-06-11 18:08:49 +0200

    Update TfgApplication.java

--- src/main/java/org/tfg/spring/tfg/TfgApplication.java
+++ src/main/java/org/tfg/spring/tfg/TfgApplication.java
@@ -10,5 +10,18 @@ public class TfgApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TfgApplication.class, args);
 	}
+		@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 
 }
