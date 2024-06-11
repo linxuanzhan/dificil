7d31dc38a28858b67bf3606c8fcd739cf804b2c9
linxuanzhan <133099133+linxuanzhan@users.noreply.github.com>
2024-06-11 18:15:34 +0200

    Update TfgApplication.java

--- src/main/java/org/tfg/spring/tfg/TfgApplication.java
+++ src/main/java/org/tfg/spring/tfg/TfgApplication.java
@@ -3,6 +3,10 @@ package org.tfg.spring.tfg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})//Bloquear login Spring Security
 public class TfgApplication {
